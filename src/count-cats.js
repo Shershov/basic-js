ab42a78d9bbf5a76d188548a441cb36854947e9e
Eugene Shershov <Absolute@tut.by>
2020-02-23 19:12:38 +0300

    feat: add count-cats and carbon-dating

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+    let countCats = 0;
+    for (let i = 0; i < backyard.length; i++) {
+        for (let k = 0; k < backyard[i].length; k++) {
+            if (backyard[i][k] === '^^') {
+                countCats++;
+            }
+        }
+    }
+    return countCats;
 };
