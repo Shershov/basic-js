97ba4f39633b2e5477e8299bc66b8cf6b37a2f22
Eugene Shershov <Absolute@tut.by>
2020-02-26 12:15:54 +0300

    feat: add what season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,16 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+    if (!date) {
+        return 'Unable to determine the time of year!';
+    }
+
+    let month = date.getUTCMonth();
+    if (month == 11 || month < 2) {
+        return 'winter';
+    } else if (month <= 4) {
+        return 'spring';
+    } else if (month <= 7) {
+        return 'summer';
+    } else if (month <= 10) {
+        return 'autumn';
+    }
 };
